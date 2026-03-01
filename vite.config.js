f16d493764f5fe825eaa90f8e453a762d448abfe
Minh Phạm <minh280804@gmail.com>
2026-03-02 04:00:29 +0700

    fix lỗi không react

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    react(), // ⚠ QUAN TRỌNG - phải có dòng này
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
